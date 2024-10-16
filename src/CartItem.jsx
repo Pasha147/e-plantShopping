2896523af798182bae7ab968276a8768459587c3
Pasha147 <pavel147223@gmail.com>
2024-10-16 17:45:17 +0300

    amount has done

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -7,9 +7,15 @@ const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
+
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+let totalAmount=0
+cart.forEach(element => {
+  const priceNumber = 
+  totalAmount += parseInt(element.cost.replace('$', ''), 10);  
+});
+ return totalAmount
   };
 
   const handleContinueShopping = (e) => {
